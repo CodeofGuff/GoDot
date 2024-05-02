d321d68cb70d81fe0876070a4bc35985339c5e42
CodeofGuff <guffcode@gmail.com>
2024-05-01 22:55:28 -0700

    Gamer Progress

--- TestGame/scripts/bullet.gd
+++ TestGame/scripts/bullet.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+var speed = 200
+const RANGE = 400
+var travelled_distance = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	%AnimatedSprite2D.play("hover")
+func _physics_process(delta):
+	%AnimatedSprite2D.play("main")
+	position += transform.x * speed * delta
 
+	
+	travelled_distance += speed * delta
+	if travelled_distance > RANGE:
+		queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_body_entered(body):
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
--- TestGame/scripts/player.gd
+++ TestGame/scripts/player.gd
@@ -1,20 +1,25 @@
 extends CharacterBody2D
 
 
+@export var bullet: PackedScene
+
 # Variables
 var speed = 225
 var gravity = 1000
 var jump_force = -600
 var jump_count = 0
 var extrajumps = 1  #if Input.is_action_just_pressed("accept"): extrajumps += 1
+var can_shoot: bool = true
 
 func _ready():
-	%AnimatedSprite2D.play("idle")
-
+	%Animations.play("idle")
+	%Smoke.play("idle")
+	
 func _physics_process(delta):
 	player_movement(delta)
 	player_jump(delta)
-
+	spawn_bullet()
+	
 	
 	# Applying gravity
 	velocity.y += gravity * delta
@@ -27,18 +32,20 @@ func player_movement(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("left"):
 		velocity.x -= speed
-		%AnimatedSprite2D.flip_h = true
-		%AnimatedSprite2D.play("run")
+		%Animations.flip_h = true
+		%Smoke.flip_h = true
+		%Animations.play("run")
 	elif Input.is_action_pressed("right"):
 		velocity.x += speed
-		%AnimatedSprite2D.flip_h = false
-		%AnimatedSprite2D.play("run")
+		%Animations.flip_h = false
+		%Smoke.flip_h = false
+		%Animations.play("run")
 		
 	# Applying gravity
 	velocity.y += gravity * delta
 	
 	if velocity.x == 0 and is_on_floor():
-		%AnimatedSprite2D.play("idle")
+		%Animations.play("idle")
 	
 	move_and_slide()
 	
@@ -50,5 +57,14 @@ func player_jump(delta):
 		
 	if is_on_floor(): 
 		jump_count = 0
-
-
+		
+func spawn_bullet():
+	if can_shoot:
+		if Input.is_action_just_pressed("attack"):
+			var bullet_instance = bullet.instantiate()
+			bullet_instance.global_transform = global_transform
+			get_parent().add_child(bullet_instance)
+			can_shoot = false
+			await get_tree().create_timer(1).timeout
+			can_shoot = true
+	
