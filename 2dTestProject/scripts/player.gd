48540d530bfadaacd0580fb3b36eb28c412e305d
CodeofGuff <guffcode@gmail.com>
2024-05-16 00:04:26 -0700

    3

--- 2dTestProject/scripts/player.gd
+++ 2dTestProject/scripts/player.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,5 +11,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	if Input.is_action_pressed("left"):
+		velocity.x += -50
+		$AnimatedSprite2D.play("walk")
+	if Input.is_action_pressed("right"):
+		velocity.x += 50
+		$AnimatedSprite2D.play("walk")
+	
+		
 
 	move_and_slide()
